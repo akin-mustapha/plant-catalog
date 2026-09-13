dea76b288bb156eb010cc1095c5857c570fe7d5c
Akinkunmi Mustapha <akinkunmimustapha1@example.com>
2026-09-13 20:58:45 +0100

    repository

--- backend/plant-catalog/repository/notification_repository.py
+++ backend/plant-catalog/repository/notification_repository.py
@@ -48,8 +48,102 @@ class NotificationRepository:
             logger.error(e)
             return None
 
-    def select_by_id(self, notification_id: str):
-        pass
+    def select_all(self):
+        logger.info("Selecting all notifications")
+        try:
+            table = self.db.Table("notification")
+            response = table.scan()
+            items = response["Items"]
+            logger.info(f"Selected {len(items)}")
 
-    def update_notification_by_id(self, notification_id: str, notification):
-        pass
+            return [
+                {k: (list(v) if isinstance(v, set) else v) for k, v in item.items()}
+                for item in items
+            ]
+        except ClientError as e:
+            logger.error(e)
+
+    def select_by_id(self, notification_id: str):
+        logger.info(f"Selecting notification by id: {notification_id}")
+        try:
+            table = self.db.Table("notification")
+            response = table.get_item(Key={"notification_id": notification_id})
+
+            item = response.get("Item")
+            if item is None:
+                logger.info(f"No notification found for id: {notification_id}")
+                return None
+
+            logger.info(f"Selected {len(item)}")
+
+            return {k: (list(v) if isinstance(v, set) else v) for k, v in item.items()}
+        except ClientError as e:
+            logger.error(e)
+            return None
+
+    def update_notification_by_id(self, notification_id: str, notification: Notification):
+        logger.info(f"Updating notification by id: {notification_id}")
+        try:
+            table = self.db.Table("notification")
+            table.update_item(
+                Key={
+                    "notification_id": notification_id
+                },
+                UpdateExpression="""
+                    set #ty=:ty,
+                    #n=:n,
+                    #d=:d,
+                    #st=:st,
+                    #c=:c,
+                    #u=:u,
+                    #sc=:sc,
+                    #i=:i,
+                    #nr=:nr,
+                    #ta=:ta
+                """,
+                ExpressionAttributeNames={
+                    "#ty": "type_id",
+                    "#n": "name",
+                    "#d": "description",
+                    "#st": "status",
+                    "#c": "contacts",
+                    "#u": "user_id",
+                    "#sc": "schedule",
+                    "#i": "interval",
+                    "#nr": "next_run_date",
+                    "#ta": "topic_arn"
+                },
+                ExpressionAttributeValues={
+                    ":ty": notification.type_id,
+                    ":n": notification.name,
+                    ":d": notification.description,
+                    ":st": notification.status,
+                    ":c": notification.contacts,
+                    ":u": notification.user_id,
+                    ":sc": notification.schedule,
+                    ":i": notification.interval,
+                    ":nr": notification.next_run_date,
+                    ":ta": notification.topic_arn
+                },
+                ConditionExpression=Attr("notification_id").exists()
+            )
+        except ClientError as e:
+            logger.error(e)
+            return {"message": "Notification update failed"}
+
+        logger.info(f"Updated {notification_id}")
+
+        return {"message": "Notification updated successfully"}
+
+    def delete_by_id(self, notification_id: str):
+        logger.info(f"Deleting notification by id: {notification_id}")
+        try:
+            table = self.db.Table("notification")
+            table.delete_item(Key={"notification_id": notification_id})
+        except ClientError as e:
+            logger.error(e)
+            return {"message": "Notification delete failed"}
+
+        logger.info(f"Deleted {notification_id}")
+
+        return {"message": "Notification deleted successfully"}
